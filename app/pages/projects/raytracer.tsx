0d94c5e48d87ddb9b164697f2fa4ab7e257ffa62
Noah Steiniger <noah.steiniger@epitech.eu>
2024-10-23 15:21:17 +0200

    feat (caca on raytracer)

--- app/pages/projects/raytracer.tsx
+++ app/pages/projects/raytracer.tsx
@@ -7,7 +7,7 @@ export default function Raytracer({ titleRef, filterRef, bgRef, }: {
     bgRef: React.RefObject<HTMLDivElement>,
 }): ReactNode {
 	return (
-		<Project titleRef={titleRef} filterRef={filterRef} bgRef={bgRef} name={"raytracer"} childrens={["caca"]} />
+		<Project titleRef={titleRef} filterRef={filterRef} bgRef={bgRef} name={"raytracer"} childrens={["caaaaaaca"]} />
 
 	)
 }
