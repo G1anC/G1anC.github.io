7f83f3e2c197a94bca546a087ab001a4c210fcc9
Noah Steiniger <noah.steiniger@epitech.eu>
2024-11-11 19:41:40 +0100

    feat(newLayout): add of layout as a client component in layout page

--- app/layoutComponent.tsx
+++ app/layoutComponent.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import gsap from "gsap";
 import localFont from "next/font/local";
+import {Dir} from "node:fs";
 
 
 const HalenoirThin = localFont({
@@ -14,6 +15,10 @@ const HalenoirThin = localFont({
 const HalenoirBlack = localFont({
     src: "../public/Halenoir-Black.otf",
 });
+const DirtyLine = localFont({
+    src: "../public/dirtyline.woff",
+});
+
 
 
 export default function LayoutComponent({ children }: Readonly<{ children: React.ReactNode }>) {
@@ -23,9 +28,9 @@ export default function LayoutComponent({ children }: Readonly<{ children: React
     const [open, setOpen] = React.useState(false);
     const tl = React.useRef<gsap.core.Timeline>();
     const lists = [
-        {name: "pages", links: ["Index", "Expertise", "Projects", "About", "Contact"]},
-        {name: "Biggest projects", links: ["AREA", "42sh", "Raytracer", "Camille_BC", "Evelyne Creations"]},
-        {name: "Socials", links: ["Instagram", "LinkedIn", "Github"]}
+        {name: "pages", links: ["indeX", "exPertise", "Projects", "abOut", "contaCt"]},
+        {name: "Biggest projects", links: ["arEa", "42sh", "rayTracer", "Camille_bC", "Eve_crea."]},
+        {name: "Socials", links: ["inSta", "LinkedIn", "gitHub"]}
     ]
 
     useGSAP(() => {
@@ -60,17 +65,17 @@ export default function LayoutComponent({ children }: Readonly<{ children: React
     return (
         <body ref={body}
             className={`w-screen h-screen ${HalenoirBlack.className} tracking-tight p-8 flex justify-center overflow-hidden text-black items-center bg-white text-xl antialiased`}>
-        <div className="menu fixed top-0 text-[#777777] uppercase right-0 w-80 z-[-10] h-full">
-            <div className="menu-wrapper p-12 flex flex-col justify-between items-start h-full">
+        <div className="menu fixed top-0 text-[#777777] right-0 w-80 h-full">
+            <div className="menu-wrapper py-8 flex flex-col justify-between items-start h-full">
                 {lists.map((list, i) => (
                     <div key={i} className="menu-list opacity-0">
-                        <div className={`lowercase text-xl tracking-widest ${HalenoirThin.className}`}>{list.name}</div>
+                        <div className={`lowercase text-xl tracking-widest ${HalenoirThin.className} mb-2`}>{list.name}</div>
                         {list.links.map((item, j) => (
-                            <div className={"uppercase text-4xl cursor-pointer flex items-start justify-start"} style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}>
+                            <div className={`${DirtyLine.className} text-5xl cursor-pointer transition-all duration-100 hover:text-white hover:ml-2 flex items-start justify-start`} style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}>
                                 <Link
                                     className="link relative"
                                     key={j}
-                                    href={`/${item.toLowerCase()}`}>
+                                    href={`/pages/${item.toLowerCase()}`}>
                                         {item}
                                 </Link>
                             </div>
@@ -84,33 +89,34 @@ export default function LayoutComponent({ children }: Readonly<{ children: React
              className={"h-full opacity-0 w-full relative z-2 rounded-2xl bg-white flex items-center flex-col justify-center border border-[#A3A3A3]"}>
             <div className={"header w-full text-[3.2rem] border-b-1 border-b border-b-[#A3A3A3]"}>
                 <div className="header-container flex h-full w-full">
-                    <Box supp={" flex-[1.25] border-r border-r-1 uppercase border-r-[#A3A3A3]"}>Portfolio</Box>
-                    <Box supp={" flex-[0.25] border-r border-r-1 border-r-[#A3A3A3]"}>
+                    <Box supp={` flex-[1.25] border-r border-r-1 text-3xl text-indigo-600 border-r-[#A3A3A3] ${DirtyLine.className}`}>pOrTfoLiO</Box>
+                    <Box supp={"flex-[0.25] border-r border-r-1 border-r-[#A3A3A3]"}>
                         <Medias/>
                     </Box>
-                    <button className={`p-6 h-full text-[1.2rem] flex items-center flex-[1.25] justify-end uppercase`} onClick={() => {
-                        const tl = gsap.timeline();
+                    <div className={"w-full h-full flex-[1.25] flex items-center justify-end"}>
+                        <button className={`w-full p-6 h-full ${DirtyLine.className} text-3xl flex items-center flex-[1.25] text-indigo-600 hover:text-indigo-400 hover:mr-2 transition-all duration-100 justify-end`} onClick={() => {
+                            const tl = gsap.timeline();
 
-                        if (!open) {
-                            gsap.set(".link", {y: 200});
-                            tl
-                                .to(container.current, {translateX: "-20rem", duration: 1, ease: "power4.inOut"}, "<")
-                                .to(body.current, {backgroundColor: "#111111", duration: 1, ease: "power4.inOut"}, "<")
-                                .to(".link", {y: 0, stagger: 0.05, duration: 0.5, delay: -0.5})
-                                .to(".menu-list", {opacity: 1, ease: "power4.inOut"}, "<")
-                        } else {
-                            tl
-                                .to(".link", {y: -100, stagger: 0.05, duration: 0.5})
-                                .to(".menu-list", {opacity: 0, ease: "power4.inOut"})
-                                .to(container.current, {translateX: "0rem", duration: 1, ease: "power4.inOut"}, "<")
-                                .to(body.current, {backgroundColor: "#ffffff", duration: 1, ease: "power4.inOut"}, "<")
-                        }
-                        setOpen(!open)
-
-                        tl.play()
-                    }}>
-                        Menu
-                    </button>
+                            if (!open) {
+                                gsap.set(".link", {y: 200});
+                                tl
+                                    .to(container.current, {translateX: "-20rem", duration: 1, ease: "power4.inOut"}, "<")
+                                    .to(body.current, {backgroundColor: "#222222", duration: 1, ease: "power4.inOut"}, "<")
+                                    .to(".link", {y: 0, stagger: 0.05, duration: 0.5, delay: -0.5})
+                                    .to(".menu-list", {opacity: 1, ease: "power4.inOut"}, "<")
+                            } else {
+                                tl
+                                    .to(".link", {y: -100, stagger: 0.05, duration: 0.5})
+                                    .to(".menu-list", {opacity: 0, ease: "power4.inOut"}, "<")
+                                    .to(container.current, {translateX: "0rem", duration: 1, ease: "power4.inOut"}, "<")
+                                    .to(body.current, {backgroundColor: "#ffffff", duration: 1, ease: "power4.inOut"}, "<")
+                            }
+                            setOpen(!open)
+                            tl.play()
+                        }}>
+                            mEnU
+                        </button>
+                    </div>
                 </div>
             </div>
             <div ref={content} className={"children w-full h-full"} style={{
