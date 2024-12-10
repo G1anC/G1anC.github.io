af8934de720cfc30d6743926522edf0aeb3d979d
Noah Steiniger <noah.steiniger@epitech.eu>
2024-12-10 18:19:26 +0100

    feat (projects): add of front landing page and finish of Evelyne's part

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -4,7 +4,9 @@ import React, {useEffect, useState, useRef} from "react";
 import localFont from "next/font/local";
 import {gsap} from "gsap";
 import LayoutComponent from "@/app/components/layoutComponent";
-import {useGSAP} from "@gsap/react";
+import InfoBlock from "@/app/components/InfoBlock";
+import FluidBack from "@/app/components/FluidBack";
+
 
 const DirtyLine = localFont({src: "../../../public/dirtyline.woff"});
 const HalenoirDemi = localFont({src: "../../../public/Halenoir-DemiBold.otf"});
@@ -674,11 +676,11 @@ const projects = [
     },
     {
         title: "aVen inC.",
-        secondTitle: "deVign inC. website",
+        secondTitle: "aVen inC.",
         content: [
             <Block side={Side.CENTER}>
-                <div className={"txt"}>Aven Inc.</div>
-                <div className={"txt"}>in coming</div>
+                <div className={"txt"}>my future agency's website</div>
+                <div className={"txt"}>in coming...</div>
             </Block>,
         ],
 
@@ -687,68 +689,72 @@ const projects = [
 
 export default function Projects() {
     const tl = useRef<gsap.core.Timeline>()
-    const [active, setActive] = useState<number>(2);
-    const projectRef = useRef(null);
+    const [active, setActive] = useState<number>(-1);
+    const projectRef = useRef<HTMLDivElement | null>(null);
     const dontTouch = useRef(null);
     const [open, setOpen] = useState(false);
-    let menuTl = gsap.timeline();
-    let menuCloseTl = gsap.timeline();
+    const menu = useRef(null);
+    const link = useRef(null);
+    let menuTl = useRef<gsap.core.Timeline>();
+    let menuCloseTl = useRef<gsap.core.Timeline>();
     const projectContentRef = useRef(null);
+    const filter = useRef(null);
 
     useEffect(() => {
         gsap.set(".link", { y: 200 });
-        menuTl = gsap.timeline({ paused: true })
-            .to(".menu", { left: "2px", ease: "power2.out" })
+        menuTl.current = gsap.timeline({ paused: true })
+            .to(menu.current, { left: "2px", ease: "power2.out" })
             .to(".link", { y: 0, stagger: 0.05, duration: 0.5, delay: -1 });
-    
-        menuCloseTl = gsap.timeline({ paused: true })
-            .to(".link", { y: 200, stagger: 0.05, duration: 0.5 })
-            .to(".menu", { left: "-400px", ease: "power2.out", delay: "-1" });
-    
-    }, []);
-    
-    React.useEffect(() => {
-        if (!menuTl.current || !menuCloseTl.current) return;
-        if (open) {
-            if (!menuTl.current.isActive()) {
-                menuTl.current.play();
-            }
-        } else {
-            if (!menuCloseTl.current.isActive()) {
-                menuCloseTl.current.play();
-            }
-        }
-    }, [open]);
 
-    useEffect(() => {
+        menuCloseTl.current = gsap.timeline({ paused: true })
+            .to(".link", { y: 200, stagger: 0.05, duration: 0.5 })
+            .to(menu.current, { left: "-400px", ease: "power2.out", delay: -1 });
+
+        if (tl.current) tl.current.play();
+        filter.current && gsap.fromTo(filter.current, { opacity: 0}, { opacity: 1, duration: 0.5, delay: 1  });
+
+        if (active === -1) return;
         tl.current = gsap.timeline()
             .to(".titleLetters", {duration: 1, opacity: 1, y: 0, stagger: 0.05, delay: 1, ease: "power4.inOut"})
             .to(".titleLetters", {opacity: 0, y: 10, stagger: 0.05, duration: 1, ease: "power4.inOut"})
             .to(".menu-items", {opacity: 1, stagger: 0.1, duration: 0.5})
             .to("project-image", {opacity: 1, duration: 0.5})
             .to(dontTouch.current, {display: "none"}, "<");
-    }, [])
-
-    const toggleMenu = (): void => {
-        setOpen(!open) }
+    }, []);
+    
+    useEffect(() => {
+        if (!menuTl.current) return;
+        console.log("animation")
+        if (open) {
+            if (!menuTl.current.isActive()) {
+                menuTl.current.play();
+                return
+            }
+        }
+            menuTl.current.reverse();
+    }, [open]);
 
     return (
         <LayoutComponent name={"Projects"}>
             <div className={"w-full flex h-full items-center justify-center relative"}>
-                    <div className="menu left-[-400px] absolute w-80 top-2 bg-white rounded-xl z-10 border border-[#A3A3A3]" style={{ boxShadow: "10px 10px 20px #00000020" }}>
+                    <div ref={menu} className="menu left-[-400px] absolute w-80 top-2 bg-white rounded-xl z-10 border border-[#A3A3A3]" style={{ boxShadow: "10px 10px 20px #00000020" }}>
                         <div className="p-4 flex flex-col relative items-end justify-end w-full">
-                            <button className={"w-8 h-8 absolute top-4 right-4"} onClick={toggleMenu}>
+                            <button className={"w-8 h-8 absolute top-4 right-4"} onClick={() => {setOpen(false)}}>
                                 <svg className="fill-indigo-600 hover:fill-indigo-300 duration-100 transition-all" height="32px" width="32px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490 490"> <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 "/></svg>
                             </button>
                             {projects.map((item, i) => (
-                                <div key={i} className={`w-full flex items-start justify-start ${DirtyLine.className} text-4xl text-indigo-600 `}>
+                                <div ref={link} key={i} className={`w-full flex items-start justify-start ${DirtyLine.className} text-4xl text-indigo-600 `}>
                                     <button className={`cursor-pointer transition-all duration-100 hover:pl-2 hover:text-indigo-400 text-indigo-600 hover:mr-2`}
                                         style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)", }}
                                         onClick={() => {
                                             setActive(i)
-                                            if (projectContentRef.current)
-                                                window.scrollTo(0, projectContentRef.current);
-                                            toggleMenu()
+                                            if (projectRef.current) {
+                                                projectRef.current.scrollTo({
+                                                    top: 0,
+                                                    behavior: "smooth", // Enables smooth scrolling
+                                                });
+                                            }
+                                            setOpen(false)
                                         }}
                                     >
                                         <div className={"link relative"}>{item.title}</div>
@@ -757,28 +763,64 @@ export default function Projects() {
                             ))}
                         </div>
                     </div>
-                    <button className={"menu-button bg-white p-2 top-2 left-2 absolute z-[5] rounded-xl"} onClick={toggleMenu}>
+                    <button className={"menu-button bg-white p-2 top-2 left-2 absolute border border-[#A3A3A3] z-[5] rounded-xl"} onClick={() => {setOpen(true)}}>
                         <img src={"/images/menu(1).png"} width={32} height={32} />
                     </button>
-                <div className={"w-full overflow-hidden flex flex-col items-center justify-center"} style={{height: "calc(100vh - 7rem)"}}>
-                    <div ref={projectRef} className={"w-full h-full overflow-y-scroll relative"}>
-                        <div className={"sticky z-[-1] px-48 pb-12 w-full flex items-start justify-center h-full top-0 left-0"}>
-                            <div className={"w-full h-full border-b px-2 pb-2 border-x border-[#A3A3A3] rounded-b-2xl"}>
-                                <div className={"w-full h-full rounded-b-xl"} style={{background: "linear-gradient(to bottom, rgba(79, 70, 229, 0.1), rgba(79, 70, 229, 0.5))"}}></div>
-                            </div>
-                        </div>
-                        <div className={"absolute top-0 left-0 w-full h-full"}>
-                            <div className={`w-full h-[40vh] flex flex-col items-center justify-center text-black ${DirtyLine.className} text-[140px]`}>
-                                <div className={"pt-24 pb-16 px-16 rounded-3xl bg-white border border-[#A3A3A3] text-center"}
-                                     style={{boxShadow: "10px 10px 20px #00000030"}}>{projects[active].secondTitle}
+                    {active === -1 ? 
+                        <>
+                            {/* <FluidBack /> */}
+                            <div ref={filter} className="absolute top-0 left-0  w-full h-full" style={{backgroundImage: "radial-gradient(circle, transparent, #0000ff30)" }}></div>
+                            <div className="relative p-4 w-full h-full flex flex-col items-center justify-center">
+                                <InfoBlock b={false} left={[
+                                    <div className="clipper"><div className="txt relative px-5"> </div></div>,
+                                ]} center={[
+                                    <div className="clipper"><div className="relative txt">All the code</div></div>,
+                                    <div className="clipper"><div className="relative txt">i've done</div></div>,
+                                ]} right={[
+                                    <div className="clipper">
+                                        <div className="relative txt">Within the 3 years</div>
+                                    </div>,
+                                    <div className={"clipper"}>
+                                        <div className={"relative txt"}>of my dev life</div>
+                                    </div>
+                                ]}/>
+        
+                                <div className={`w-full h-full flex items-center justify-center text-[250px] ${DirtyLine.className}`}>pRoJecTs</div>
+        
+                                <InfoBlock b={true} left={[
+                                    <div className={"relative txt"}>Click the sandwich</div>,
+                                ]} center={[
+                                    <div className="txt" key={0}><div>to see them all</div></div>
+                                ]} right={[
+                                    <div className="txt" key={0}><div>and enjoy learning from 'em</div></div>
+                                ]} />
+                            </div>                
+                        </>
+                    :
+                        <div className={"w-full overflow-hidden flex flex-col items-center justify-center"} style={{height: "calc(100vh - 7rem)"}}>
+                            <div ref={projectRef} className={"w-full h-full overflow-y-scroll relative"}>
+                                <div className={"sticky z-[-1] px-48 pb-12 w-full flex items-start justify-center h-full top-0 left-0"}>
+                                    <div className={"w-full h-full border-b px-2 pb-2 border-x border-[#A3A3A3] rounded-b-2xl"}>
+                                        <div className={"w-full h-full rounded-b-xl"} style={{background: "linear-gradient(to bottom, rgba(79, 70, 229, 0.1), rgba(79, 70, 229, 0.5))"}}></div>
+                                    </div>
+                                </div>
+                                <div className={"absolute top-0 left-0 w-full h-full"}>
+                                    <div className={`w-full h-[40vh] flex flex-col items-center justify-center text-black ${DirtyLine.className} text-[140px]`}>
+                                        <div className={"pt-24 pb-16 px-16 rounded-3xl bg-white border border-[#A3A3A3] text-center"}
+                                            style={{boxShadow: "10px 10px 20px #00000030"}}>{projects[active].secondTitle}
+                                        </div>
+                                    </div>
+                                    <div ref={projectContentRef} className={`w-full h-full sm:text-3xl ${HalenoirDemi.className} text-gray-600 text-base uppercase relative p-4 px-32`}>
+                                        {projects[active].content.map((item, i) => (
+                                            <div key={i}>
+                                                {item}
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
-                            <div ref={projectContentRef} className={`w-full h-full sm:text-3xl ${HalenoirDemi.className} text-gray-600 text-base uppercase relative p-4 px-32`}>
-                                {projects[active].content}
-                            </div>
                         </div>
-                    </div>
-                </div>
+                    }
             </div>
         </LayoutComponent>
     );
